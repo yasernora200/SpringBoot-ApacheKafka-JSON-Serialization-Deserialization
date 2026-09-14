89899281a32d8a82fe90fc27ec32b97cc2690db7
unknown <yasernora200@gmail.com>
2026-09-15 02:26:07 +0300

    Add consumer and readme

---
+++ kafka-consumer/src/main/java/com/nora/consumer/KafkaMessageListener.java
@@ -0,0 +1,33 @@
+package com.nora.consumer;
+
+import com.nora.dto.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "employees",groupId = "emp-group")
+    public void consumeEvents(Customer customer) {
+        log.info("consumer consume the events {} ", customer.toString());
+    }
+
+//    @KafkaListener(topics = "employees",groupId = "emp-group")
+//    public void consume2(Customer message) {
+//        log.info("consumer2 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "employees",groupId = "emp-group")
+//    public void consume3(Customer message) {
+//        log.info("consumer3 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "employees",groupId = "emp-group")
+//    public void consume4(Customer message) {
+//        log.info("consumer4 consume the message {} ", message);
+//    }
+}
